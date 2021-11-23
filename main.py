551ef602b5a96b45651f833fa4c10ded9dbeaeb8
bvoo <bvoowo@gmail.com>
2021-11-23 13:12:54 -0800

    gitignore, readme

--- main.py
+++ main.py
@@ -1,29 +1,3 @@
-# Blackjack Midterm Project
-# 22/11/2021
-
-# Rules for Blackjack:
-# - The game begins with the player placing a bet.
-# - The player is dealt two cards.
-# - The dealer is dealt two cards.
-# - The player is given the option to hit or stay.
-# - The dealer will hit if the dealer's score is less than 17.
-# - The dealer will stay if the dealer's score is 17 or more.
-# - The player wins if their score is higher than the dealer's.
-# - The dealer wins if their score is higher than the player's.
-# - The player wins if their score is 21.
-# - The dealer wins if their score is 21.
-# - The player wins if their score is higher than the dealer's and less than 21.
-# - The dealer wins if their score is higher than the player's and less than 21.
-# - The game is a draw if the player and dealer's scores are equal and both are 21.
-
-# TODO:
-# - Handle aces properly
-# - Handle wagers
-# - Handle save files
-# - Handle player quitting without saving
-# - Graphically display cards
-# - Allow player to enter a new game
-
 
 from random import shuffle
 from components.display import display_player, display_dealer 
